40bb5befa17e76dd830f11391ee33a81c2f0dd08
lopez-mgu <lopez.mgu@gmail.com>
2022-08-27 08:31:12 -0600

    second commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Footer.css';
+import { DiHtml5, DiCss3, DiReact } from 'react-icons/di';
+import { SiJavascript, SiBootstrap } from 'react-icons/si';
+
 
 const Footer = () => {
   return (
@@ -7,10 +10,36 @@ const Footer = () => {
         <footer id="footer">
             <div className="container">
             <div className="copyright">
-                &copy; Copyright <strong><span>iPortfolio</span></strong>
+                &copy; Copyright <strong><span>Miguel Gutierrez </span></strong>
             </div>
             <div className="credits">
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                Designed and build using:
+                <div>
+                  <div className="d-flex justify-content-center">
+                      <div className="row skills-content justify-content-center mt-4">
+                          <div className='d-flex flex-column align-items-center col col-md-2'>
+                              <DiHtml5 className="icon"  size="1.4rem" color="#149ddd"/>
+                              <p style={{fontSize: ".5rem"}} color="#050d18">HTML5</p>
+                          </div>
+                          <div className='d-flex flex-column align-items-center col'>
+                              <DiCss3 className="icon"  size="1.4rem" color="#149ddd"/>
+                              <p style={{fontSize: ".5rem"}} color="#050d18">CSS3</p>
+                          </div>
+                          <div className='d-flex flex-column align-items-center col'>
+                              <SiJavascript className="icon"  size="1.4rem" color="#149ddd"/>
+                              <p style={{fontSize: ".5rem"}} color="#050d18">JavaScript</p>
+                          </div>
+                          <div className='d-flex flex-column align-items-center col'>
+                              <DiReact className="icon"  size="1.4rem" color="#149ddd"/>
+                              <p style={{fontSize: ".5rem"}} color="#050d18">React</p>
+                          </div>
+                          <div className='d-flex flex-column align-items-center col'>
+                              <SiBootstrap className="icon"  size="1.4rem" color="#149ddd"/>
+                              <p style={{fontSize: ".5rem"}} color="#050d18">BootStrap</p>
+                          </div>
+                      </div>
+                  </div>
+                </div>
             </div>
             </div>
         </footer>
--- src/components/Navbar/Nabvar.jsx
+++ src/components/Navbar/Nabvar.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiOutlineTwitter, AiOutlineLinkedin, AiOutlineGithub, AiOutlineHome,
         AiOutlineUser, AiOutlineFile, AiOutlineBook, AiOutlineCloudServer, AiOutlineMail } from 'react-icons/ai';
-
+import { GiHamburgerMenu } from 'react-icons/gi';
 import './Navbar.css';
 
 const Nabvar = () => {
+    const NavClassMobile = 'mobile-nav-active'
+    const [ navClass, setNavClass ] = useState('');
+    console.log(navClass)
   return (
     <>
         {/* ======= Mobile nav toggle button ======= */}
-        <i className="bi bi-list mobile-nav-toggle d-xl-none"></i>
+        <div className={`mobile-nav-toggle d-xl-none icon ${navClass}`}>
+            <GiHamburgerMenu onClick={() => {navClass === NavClassMobile?setNavClass(''):setNavClass(NavClassMobile)}} size="1.8rem" color="white"/>
+        </div>
 
         {/* ======= Header ======= */}
         <header id="header">
@@ -18,9 +23,9 @@ const Nabvar = () => {
                 <img src="Images/profile-photo.jpg" alt="" className="img-fluid rounded-circle" />
                 <h1 className="text-light"><a href="index.html">Miguel Gutierrez</a></h1>
                 <div className="social-links mt-3 text-center">
-                <a href="#" className="twitter"><AiOutlineTwitter/></a>
-                <a href="#" className="facebook"><AiOutlineLinkedin/></a>
-                <a href="#" className="instagram"><AiOutlineGithub/></a>
+                <a href="https://twitter.com/lopezmgudev" className="twitter"><AiOutlineTwitter className='icon'/></a>
+                <a href="https://www.linkedin.com/in/miguel-angel-gutierrez-lopez-384116175/" className="linkedin"><AiOutlineLinkedin className='icon'/></a>
+                <a href="https://github.com/lopez-mgu" className="GitHub"><AiOutlineGithub className='icon'/></a>
                 {/* <a href="#" className="google-plus"><i className="bx bxl-skype"></i></a>
                 <a href="#" className="linkedin"><i className="bx bxl-linkedin"></i></a> */}
                 </div>
@@ -28,12 +33,12 @@ const Nabvar = () => {
 
             <nav id="navbar" className="nav-menu navbar">
                 <ul>
-                    <li><a href="#hero" className="nav-link scrollto"><AiOutlineHome/><span>Home</span></a></li>
-                    <li><a href="#about" className="nav-link scrollto"><AiOutlineUser/> <span>About</span></a></li>
-                    <li><a href="#resume" className="nav-link scrollto"><AiOutlineFile/> <span>Resume</span></a></li>
-                    <li><a href="#portfolio" className="nav-link scrollto"><AiOutlineBook/> <span>Portfolio</span></a></li>
-                    <li><a href="#services" className="nav-link scrollto"><AiOutlineCloudServer/> <span>Services</span></a></li>
-                    <li><a href="#contact" className="nav-link scrollto"><AiOutlineMail/> <span>Contact</span></a></li>
+                    <li><a href="#hero" className="nav-link scrollto"><AiOutlineHome className='icon'/><span>Home</span></a></li>
+                    <li><a href="#about" className="nav-link scrollto"><AiOutlineUser className='icon'/> <span>About</span></a></li>
+                    <li><a href="#resume" className="nav-link scrollto"><AiOutlineFile className='icon'/> <span>Resume</span></a></li>
+                    <li><a href="#portfolio" className="nav-link scrollto"><AiOutlineBook className='icon'/> <span>Portfolio</span></a></li>
+                    {/* <li><a href="#services" className="nav-link scrollto"><AiOutlineCloudServer className='icon'/> <span>Services</span></a></li> */}
+                    <li><a href="#contact" className="nav-link scrollto"><AiOutlineMail className='icon'/> <span>Contact</span></a></li>
                 </ul>
             </nav>
             </div>
