b1ceb36f199ee3a81b87aff927cf457692ecb94d
lopez-mgu <lopez.mgu@gmail.com>
2022-08-27 10:22:09 -0600

    responsive sidenavbar

--- src/components/Badges/Badges.jsx
+++ src/components/Badges/Badges.jsx
@@ -26,22 +26,30 @@ const Badges = () => {
                     <p>Below you will find badges adquired.</p>
                 </div>
                 <div className='d-flex'>
-                    <div className="row w-100">
+                    <div className="row  m-auto">
 
-                        <div className="col w-25 p-3" data-aos="fade-up" data-aos-delay="300">
-                            <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="75e9b083-1fd9-4326-a03e-39c34d672a49" data-share-badge-host="https://www.credly.com"></div>
+                        <div className="col-6 col-md mx-auto" data-aos="fade-up" data-aos-delay="300">
+                            <div className="row justify-content-center">
+                                <div data-iframe-width="180" data-iframe-height="270" data-share-badge-id="75e9b083-1fd9-4326-a03e-39c34d672a49" data-share-badge-host="https://www.credly.com"></div>
+                            </div>
                         </div>
 
-                        <div className="col w-25 p-3" data-aos="fade-up" data-aos-delay="200">
-                            <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="7e74e6c0-4f2b-4c57-a9ea-6763523b6208" data-share-badge-host="https://www.credly.com"></div>
+                        <div className="col-6 col-md mx-auto" data-aos="fade-up" data-aos-delay="200">
+                            <div className="row justify-content-center">
+                                <div data-iframe-width="180" data-iframe-height="270" data-share-badge-id="7e74e6c0-4f2b-4c57-a9ea-6763523b6208" data-share-badge-host="https://www.credly.com"></div>
+                            </div>
                         </div>
 
-                        <div className="col w-25 p-3" data-aos="fade-up" data-aos-delay="100">
-                            <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="bb6504e3-921e-4ffe-b460-78b55821897c" data-share-badge-host="https://www.credly.com"></div>
+                        <div className="col-6 col-md mx-auto" data-aos="fade-up" data-aos-delay="100">
+                            <div className="row justify-content-center">
+                                <div data-iframe-width="180" data-iframe-height="270" data-share-badge-id="bb6504e3-921e-4ffe-b460-78b55821897c" data-share-badge-host="https://www.credly.com"></div>
+                            </div>
                         </div>
 
-                        <div className="col w-25 p-3" data-aos="fade-up" data-aos-delay="400">
-                            <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="3476b5ad-ba46-40ba-af37-d2a8eb85f719" data-share-badge-host="https://www.credly.com"></div>
+                        <div className="col-6 col-md mx-auto" data-aos="fade-up" data-aos-delay="400">
+                            <div className="row justify-content-center">
+                                <div data-iframe-width="180" data-iframe-height="270" data-share-badge-id="3476b5ad-ba46-40ba-af37-d2a8eb85f719" data-share-badge-host="https://www.credly.com"></div>
+                            </div>
                         </div>
                         
                     </div>
--- src/components/Navbar/Nabvar.jsx
+++ src/components/Navbar/Nabvar.jsx
@@ -12,49 +12,52 @@ const Nabvar = () => {
     const handleToggle = () => {
         setNavbarOpen(!navbarOpen)
       }
-
+      console.log(navbarOpen)
   return (
     <>
         {/* ======= Mobile nav toggle button ======= */}
-        <div className={`mobile-nav-toggle d-xl-none icon ${navbarOpen ? " showMenu" : ""}`}>
-            {/* <GiHamburgerMenu onClick={() => {navClass === NavClassMobile?setNavClass(''):setNavClass(NavClassMobile)}} size="1.8rem" color="white"/> */}
-            <button className="ham-button" onClick={handleToggle}>
-                {navbarOpen ? (
-                    <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
-                ) : (
-                    <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
-                )}
-            </button>
+        <div className={`${navbarOpen ? " showMenu" : ""}`}>
+            <div className={`mobile-nav-toggle d-xl-none icon`}>
+                {/* <GiHamburgerMenu onClick={() => {navClass === NavClassMobile?setNavClass(''):setNavClass(NavClassMobile)}} size="1.8rem" color="white"/> */}
+                <button className="ham-button" onClick={handleToggle}>
+                    {navbarOpen ? (
+                        <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+                    ) : (
+                        <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+                    )}
+                </button>
+            </div>
+
+            {/* ======= Header ======= */}
+            <header id="header">
+                <div className="d-flex flex-column">
+
+                <div className="profile">
+                    <img src="Images/profile-photo.jpg" alt="" className="img-fluid rounded-circle" />
+                    <h1 className="text-light"><a href="index.html">Miguel Gutierrez</a></h1>
+                    <div className="social-links mt-3 text-center">
+                    <a href="https://twitter.com/lopezmgudev" className="twitter"><AiOutlineTwitter className='icon'/></a>
+                    <a href="https://www.linkedin.com/in/miguel-angel-gutierrez-lopez-384116175/" className="linkedin"><AiOutlineLinkedin className='icon'/></a>
+                    <a href="https://github.com/lopez-mgu" className="GitHub"><AiOutlineGithub className='icon'/></a>
+                    {/* <a href="#" className="google-plus"><i className="bx bxl-skype"></i></a>
+                    <a href="#" className="linkedin"><i className="bx bxl-linkedin"></i></a> */}
+                    </div>
+                </div>
+
+                <nav id="navbar" className="nav-menu navbar">
+                    <ul>
+                        <li><a href="#hero" className="nav-link scrollto"><AiOutlineHome className='icon'/><span>Home</span></a></li>
+                        <li><a href="#about" className="nav-link scrollto"><AiOutlineUser className='icon'/> <span>About</span></a></li>
+                        <li><a href="#resume" className="nav-link scrollto"><AiOutlineFile className='icon'/> <span>Resume</span></a></li>
+                        <li><a href="#portfolio" className="nav-link scrollto"><AiOutlineBook className='icon'/> <span>Portfolio</span></a></li>
+                        {/* <li><a href="#services" className="nav-link scrollto"><AiOutlineCloudServer className='icon'/> <span>Services</span></a></li> */}
+                        <li><a href="#contact" className="nav-link scrollto"><AiOutlineMail className='icon'/> <span>Contact</span></a></li>
+                    </ul>
+                </nav>
+                </div>
+            </header>
         </div>
 
-        {/* ======= Header ======= */}
-        <header id="header">
-            <div className="d-flex flex-column">
-
-            <div className="profile">
-                <img src="Images/profile-photo.jpg" alt="" className="img-fluid rounded-circle" />
-                <h1 className="text-light"><a href="index.html">Miguel Gutierrez</a></h1>
-                <div className="social-links mt-3 text-center">
-                <a href="https://twitter.com/lopezmgudev" className="twitter"><AiOutlineTwitter className='icon'/></a>
-                <a href="https://www.linkedin.com/in/miguel-angel-gutierrez-lopez-384116175/" className="linkedin"><AiOutlineLinkedin className='icon'/></a>
-                <a href="https://github.com/lopez-mgu" className="GitHub"><AiOutlineGithub className='icon'/></a>
-                {/* <a href="#" className="google-plus"><i className="bx bxl-skype"></i></a>
-                <a href="#" className="linkedin"><i className="bx bxl-linkedin"></i></a> */}
-                </div>
-            </div>
-
-            <nav id="navbar" className="nav-menu navbar">
-                <ul>
-                    <li><a href="#hero" className="nav-link scrollto"><AiOutlineHome className='icon'/><span>Home</span></a></li>
-                    <li><a href="#about" className="nav-link scrollto"><AiOutlineUser className='icon'/> <span>About</span></a></li>
-                    <li><a href="#resume" className="nav-link scrollto"><AiOutlineFile className='icon'/> <span>Resume</span></a></li>
-                    <li><a href="#portfolio" className="nav-link scrollto"><AiOutlineBook className='icon'/> <span>Portfolio</span></a></li>
-                    {/* <li><a href="#services" className="nav-link scrollto"><AiOutlineCloudServer className='icon'/> <span>Services</span></a></li> */}
-                    <li><a href="#contact" className="nav-link scrollto"><AiOutlineMail className='icon'/> <span>Contact</span></a></li>
-                </ul>
-            </nav>
-            </div>
-        </header>
     </>
   )
 }
